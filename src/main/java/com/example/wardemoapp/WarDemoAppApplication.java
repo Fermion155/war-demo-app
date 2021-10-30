ac97ac666eb9caefc013ef0c13835dd5aeb05182
Paulius Drabuzinskis <pdrabuzinskis@gmail.com>
2021-10-30 16:09:39 +0300

    first commit

---
+++ src/main/java/com/example/wardemoapp/WarDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.wardemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wardemoapp/WarDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wardemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
